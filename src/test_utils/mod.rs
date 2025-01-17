2f15b92be93a098f83fe7aea3105faf1c8a1ee7a
Lhcfl <lhcfl@outlook.com>
2025-01-17 11:21:00 +0800

    rw tests

--- src/test_utils/mod.rs
+++ src/test_utils/mod.rs
@@ -1,87 +1,82 @@
 #[cfg(test)]
-use crate::globals::BOT_USERNAME;
-#[cfg(test)]
-use chrono::{DateTime, Utc};
-#[cfg(test)]
-use std::sync::LazyLock;
-#[cfg(test)]
-use teloxide_core::types::*;
-#[cfg(test)]
-use teloxide_core::Bot;
+pub mod fabricator {
+    use crate::globals::BOT_USERNAME;
+    use chrono::{DateTime, Utc};
+    use std::sync::LazyLock;
+    use teloxide_core::types::*;
+    use teloxide_core::Bot;
 
-#[cfg(test)]
-pub static TEST_BOT: LazyLock<Bot> = LazyLock::new(|| {
-    BOT_USERNAME
-        .set(String::from("testbot"))
-        .expect("should able to set");
-    Bot::new("AAAAA:ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
-});
+    pub static TEST_BOT: LazyLock<Bot> = LazyLock::new(|| {
+        BOT_USERNAME
+            .set(String::from("testbot"))
+            .expect("should able to set");
+        Bot::new("AAAAA:ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
+    });
 
-#[cfg(test)]
-pub fn fake_chat() -> Chat {
-    Chat {
-        id: ChatId(5678),
-        kind: ChatKind::Public(ChatPublic {
-            title: Some("fake chat".to_string()),
-            kind: PublicChatKind::Group(PublicChatGroup { permissions: None }),
-            description: Some("fake chat description".to_string()),
-            invite_link: None,
-            has_protected_content: None,
-        }),
-        photo: None,
-        available_reactions: None,
-        pinned_message: None,
-        message_auto_delete_time: None,
-        has_hidden_members: false,
-        has_aggressive_anti_spam_enabled: false,
-        chat_full_info: ChatFullInfo {
-            accent_color_id: None,
-            background_custom_emoji_id: None,
-            profile_accent_color_id: None,
-            profile_background_custom_emoji_id: None,
-            emoji_status_custom_emoji_id: None,
-            emoji_status_expiration_date: None,
-            has_visible_history: false,
-        },
-    }
-}
-
-#[cfg(test)]
-pub fn fake_text_message(text: &str) -> Message {
-    Message {
-        id: MessageId(1234),
-        thread_id: None,
-        from: None,
-        sender_chat: None,
-        date: DateTime::<Utc>::from_timestamp(0, 0).unwrap(),
-        chat: fake_chat(),
-        is_topic_message: false,
-        via_bot: None,
-        kind: MessageKind::Common(MessageCommon {
-            author_signature: None,
-            forward_origin: None,
-            reply_to_message: None,
-            external_reply: None,
-            quote: None,
-            edit_date: None,
-            media_kind: MediaKind::Text(MediaText {
-                text: text.to_string(),
-                entities: Vec::new(),
-                link_preview_options: None,
+    pub fn fab_chat() -> Chat {
+        Chat {
+            id: ChatId(5678),
+            kind: ChatKind::Public(ChatPublic {
+                title: Some("fake chat".to_string()),
+                kind: PublicChatKind::Group(PublicChatGroup { permissions: None }),
+                description: Some("fake chat description".to_string()),
+                invite_link: None,
+                has_protected_content: None,
             }),
-            reply_markup: None,
-            is_automatic_forward: false,
-            has_protected_content: false,
-        }),
+            photo: None,
+            available_reactions: None,
+            pinned_message: None,
+            message_auto_delete_time: None,
+            has_hidden_members: false,
+            has_aggressive_anti_spam_enabled: false,
+            chat_full_info: ChatFullInfo {
+                accent_color_id: None,
+                background_custom_emoji_id: None,
+                profile_accent_color_id: None,
+                profile_background_custom_emoji_id: None,
+                emoji_status_custom_emoji_id: None,
+                emoji_status_expiration_date: None,
+                has_visible_history: false,
+            },
+        }
+    }
+
+    pub fn fab_text_message(text: &str) -> Message {
+        Message {
+            id: MessageId(1234),
+            thread_id: None,
+            from: None,
+            sender_chat: None,
+            date: DateTime::<Utc>::from_timestamp(0, 0).unwrap(),
+            chat: fab_chat(),
+            is_topic_message: false,
+            via_bot: None,
+            kind: MessageKind::Common(MessageCommon {
+                author_signature: None,
+                forward_origin: None,
+                reply_to_message: None,
+                external_reply: None,
+                quote: None,
+                edit_date: None,
+                media_kind: MediaKind::Text(MediaText {
+                    text: text.to_string(),
+                    entities: Vec::new(),
+                    link_preview_options: None,
+                }),
+                reply_markup: None,
+                is_automatic_forward: false,
+                has_protected_content: false,
+            }),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::test_utils::*;
+    use crate::test_utils::fabricator::*;
 
     #[test]
     fn fake_message_tests() {
-        assert_eq!(fake_text_message("hello").text(), Some("hello"));
+        assert_eq!(fab_text_message("hello").text(), Some("hello"));
     }
 }
