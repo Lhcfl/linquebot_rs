e39eae383b02e0d44eced13f92f87b1319e6289e
Lhcfl <lhcfl@outlook.com>
2025-01-25 18:04:22 +0800

    refa to use pattern
    Co-authored-by: senioria <91khr@noreply.github.com>

--- src/linquebot/msg_context.rs
+++ src/linquebot/msg_context.rs
@@ -1,5 +1,13 @@
-use teloxide_core::types::Message;
+use teloxide_core::{
+    payloads::SendMessage,
+    prelude::*,
+    requests::JsonRequest,
+    types::{ChatId, Message, MessageId, ParseMode, ReplyParameters},
+};
 
+use super::{App, ModuleDesctiption};
+
+#[derive(PartialEq, Eq, Debug)]
 pub struct CmdParts<'a> {
     cmd: &'a str,
     username: Option<&'a str>,
@@ -8,46 +16,143 @@ pub struct CmdParts<'a> {
 
 impl<'a> CmdParts<'a> {
     pub fn parse_from(msg: &'a Message) -> Option<CmdParts<'a>> {
+        use crate::utils::pattern::*;
         let text = msg.text()?;
-        let mut chars = text.chars();
-        let Some('/') = chars.next() else {
+        let (content, (_, cmd, username)) = (
+            "/",
+            of_pred(|c| !c.is_whitespace() && c != '@'),
+            maybe(("@", of_pred(|c| !c.is_whitespace()))),
+        )
+            .check_pattern(text)?;
+        if cmd.is_empty() {
             return None;
-        };
-        let mut cmd_idx: usize = 0;
-        let mut idx: usize = 1;
-        for ch in chars {
-            if ch == '@' {
-                cmd_idx = idx;
-            }
-            if ch.is_whitespace() {
-                break;
-            }
-            idx += 1;
         }
-        if cmd_idx == 0 {
-            Some(CmdParts {
-                cmd: &text[1..idx],
-                username: None,
-                content: &text[idx..],
-            })
-        } else {
-            Some(CmdParts {
-                cmd: &text[1..cmd_idx],
-                username: Some(&text[cmd_idx..idx]),
-                content: &text[idx..],
-            })
-        }
+        Some(CmdParts {
+            cmd,
+            username: username.map(|u| u.1),
+            content: content.trim(),
+        })
     }
 }
 
-pub struct MsgContext<'a> {
-    cmd: Option<CmdParts<'a>>,
+pub struct Context<'a> {
+    pub cmd: Option<CmdParts<'a>>,
+    pub chat_id: ChatId,
+    pub message_id: MessageId,
+    pub app: &'static App,
 }
 
-impl<'a> MsgContext<'a> {
-    pub fn new(msg: &'a Message) -> MsgContext<'a> {
-        return MsgContext {
-            cmd: CmdParts::parse_from(msg),
+impl<'a> Context<'a> {
+    pub fn task(&self) -> TaskContext {
+        TaskContext {
+            message_id: self.message_id,
+            chat_id: self.chat_id,
+            app: self.app,
+        }
+    }
+
+    pub fn matches_command(&self, desc: &ModuleDesctiption) -> bool {
+        let Some(cmd) = &self.cmd else {
+            return false;
         };
+        cmd.cmd == desc.name
+    }
+}
+
+pub struct TaskContext {
+    chat_id: ChatId,
+    message_id: MessageId,
+    app: &'static App,
+}
+
+impl TaskContext {
+    pub fn reply(&self, text: &str) -> JsonRequest<SendMessage> {
+        self.app
+            .bot
+            .send_message(self.chat_id, text)
+            .reply_parameters(ReplyParameters::new(self.message_id))
+    }
+
+    pub fn reply_markdown(&self, text: &str) -> JsonRequest<SendMessage> {
+        self.reply(text).parse_mode(ParseMode::MarkdownV2)
+    }
+
+    pub fn reply_html(&self, text: &str) -> JsonRequest<SendMessage> {
+        self.reply(text).parse_mode(ParseMode::Html)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{msg_context::CmdParts, test_utils::fabricator::fab_text_message};
+
+    #[test]
+    fn cmd_parts_new_test() {
+        assert_eq!(CmdParts::parse_from(&fab_text_message("1")), None);
+        assert_eq!(CmdParts::parse_from(&fab_text_message("/")), None);
+        assert_eq!(
+            CmdParts::parse_from(&fab_text_message("/a")),
+            Some(CmdParts {
+                cmd: "a",
+                username: None,
+                content: "",
+            })
+        );
+        assert_eq!(
+            CmdParts::parse_from(&fab_text_message("/test")),
+            Some(CmdParts {
+                cmd: "test",
+                username: None,
+                content: "",
+            })
+        );
+        assert_eq!(
+            CmdParts::parse_from(&fab_text_message("/test args")),
+            Some(CmdParts {
+                cmd: "test",
+                username: None,
+                content: "args",
+            })
+        );
+        assert_eq!(
+            CmdParts::parse_from(&fab_text_message("/test  args more spaces")),
+            Some(CmdParts {
+                cmd: "test",
+                username: None,
+                content: "args more spaces",
+            })
+        );
+        assert_eq!(
+            CmdParts::parse_from(&fab_text_message("/test@somebot  args more spaces")),
+            Some(CmdParts {
+                cmd: "test",
+                username: Some("somebot"),
+                content: "args more spaces",
+            })
+        );
+        assert_eq!(
+            CmdParts::parse_from(&fab_text_message("/test@@strange  args more spaces")),
+            Some(CmdParts {
+                cmd: "test",
+                username: Some("@strange"),
+                content: "args more spaces",
+            })
+        );
+        assert_eq!(
+            CmdParts::parse_from(&fab_text_message("/中文 args   ")),
+            Some(CmdParts {
+                cmd: "中文",
+                username: None,
+                content: "args",
+            })
+        );
+        assert_eq!(
+            CmdParts::parse_from(&fab_text_message("/揉@somebot")),
+            Some(CmdParts {
+                cmd: "揉",
+                username: Some("somebot"),
+                content: "",
+            })
+        );
     }
 }
