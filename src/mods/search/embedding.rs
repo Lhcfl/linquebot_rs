014c8bc292334e75fe4bec80b1634caaf3e6729f
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-06-07 12:54:48 +0800

    chore(mods): :art: use `ort=2.0.0-rc.10` with mutex

--- src/mods/search/embedding.rs
+++ src/mods/search/embedding.rs
@@ -3,10 +3,14 @@ use hf_hub::{
     api::sync::{Api, ApiBuilder},
     Repo, RepoType,
 };
-use ndarray::{Array1, Axis, Ix1};
-use ort::session::{builder::GraphOptimizationLevel, Session};
+use ndarray::Ix1;
+use ort::{
+    session::{builder::GraphOptimizationLevel, Session},
+    value::TensorRef,
+};
 use std::sync::LazyLock;
 use tokenizers::Tokenizer;
+use tokio::sync::Mutex;
 
 static MODEL_ID: &str = "Snowflake/snowflake-arctic-embed-l-v2.0";
 static REVISION: &str = "main";
@@ -28,7 +32,7 @@ fn get_tokenizer() -> Result<Tokenizer> {
     Ok(tokenizer)
 }
 
-fn get_session() -> Result<Session> {
+fn get_session() -> Result<Mutex<Session>> {
     let repo = Repo::with_revision(MODEL_ID.to_string(), RepoType::Model, REVISION.to_string());
     let api = Api::new()?;
     let api = api.repo(repo.to_owned());
@@ -37,31 +41,33 @@ fn get_session() -> Result<Session> {
         .with_optimization_level(GraphOptimizationLevel::Level3)?
         .with_intra_threads(4)?
         .commit_from_file(onnx_filename)?;
-    Ok(session)
+    Ok(Mutex::new(session))
 }
 
 static TOKENIZER: LazyLock<Result<Tokenizer>> = LazyLock::new(get_tokenizer);
-static SESSION: LazyLock<Result<Session>> = LazyLock::new(get_session);
+static SESSION: LazyLock<Result<Mutex<Session>>> = LazyLock::new(get_session);
 
 pub async fn text_embedding(text: impl Into<String>) -> Result<Vec<f32>> {
     let tokenizer = TOKENIZER.as_ref().map_err(E::msg)?;
-    let session = SESSION.as_ref().map_err(E::msg)?;
+    let session_mutex = SESSION.as_ref().map_err(E::msg)?;
+    let mut session = session_mutex.lock().await;
     let encoding = tokenizer.encode(text.into(), true).map_err(E::msg)?;
     let tokens = encoding
         .get_ids()
         .iter()
-        .map(|&t| t as i64)
+        .map(|&t| t.into())
         .collect::<Vec<i64>>();
     let attention_mask = encoding
         .get_attention_mask()
         .iter()
-        .map(|&m| m as i64)
+        .map(|&m| m.into())
         .collect::<Vec<i64>>();
-    let tokens = Array1::from_vec(tokens).insert_axis(Axis(0));
-    let attention_mask = Array1::from_vec(attention_mask).insert_axis(Axis(0));
-    let outputs = session.run(ort::inputs![tokens, attention_mask]?)?;
+    let tokens = TensorRef::from_array_view(([1, tokens.len()], tokens.as_slice()))?;
+    let attention_mask =
+        TensorRef::from_array_view(([1, attention_mask.len()], attention_mask.as_slice()))?;
+    let outputs = session.run(ort::inputs![tokens, attention_mask])?;
     let embeddings = outputs[1]
-        .try_extract_tensor()?
+        .try_extract_array()?
         .squeeze()
         .into_dimensionality::<Ix1>()?
         .to_vec();
